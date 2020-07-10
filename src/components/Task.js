3e51959f684c42757fd0e6243b131c33de6f9ce0
Félix Vicente <felixvicent1306@gmail.com>
2020-07-10 14:55:57 -0300

    Customize tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default ({ desc, estimateAt, doneAt }) => {
    return (
-    <View>
+    <View style={ styles.container } >
       <Text>{ desc }</Text>
       <Text>{ estimateAt + "" }</Text>
       <Text>{ doneAt + "" }</Text>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    borderColor: '#aaa',
+    borderBottomWidth: 1,
+    alignItems: 'center',
+    paddingVertical: 10,
+  }
+});
