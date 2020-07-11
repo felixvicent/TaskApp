8e9283db20b6ce7042740293bed5a23f5ca97c76
Félix Vicente <felixvicent1306@gmail.com>
2020-07-11 13:07:00 -0300

    Customize done signal

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,14 +1,32 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import commonStyles from '../commonStyles';
 
 export default ({ desc, estimateAt, doneAt }) => {
-   return (
-    <View style={ styles.container } >
-      <Text>{ desc }</Text>
-      <Text>{ estimateAt + "" }</Text>
-      <Text>{ doneAt + "" }</Text>
+  return (
+    <View style={styles.container} >
+      <View style={styles.checkContainer} >
+        {getCheckView(doneAt)}
+      </View>
+      <View>
+        <Text>{desc}</Text>
+        <Text>{estimateAt + ""}</Text>
+      </View>
     </View>
   )
+}
+
+function getCheckView(doneAt) {
+  if (doneAt !== null) {
+    return (
+      <View style={styles.done} >
+        <Icon name='check' size={ 20 } color='#fff' />
+      </View>
+    )
+  }
+  return <View style={styles.pending}/>;
 }
 
 const styles = StyleSheet.create({
@@ -18,5 +36,25 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     alignItems: 'center',
     paddingVertical: 10,
+  },
+  checkContainer: {
+    width: '20%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  pending: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    borderWidth: 1,
+    borderColor: '#555',
+  },
+  done: {
+    height: 25,
+    width: 25,
+    borderRadius: 13,
+    backgroundColor: '#4d7031',
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
