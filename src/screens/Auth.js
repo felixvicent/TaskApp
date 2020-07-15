c1c85bd3405ca3ade6528dcfdfa51ee5f953cc74
Félix Vicente <felixvicent1306@gmail.com>
2020-07-15 13:42:43 -0300

    Create auth form

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
-import { ImageBackground, Text, StyleSheet } from 'react-native';
+import { ImageBackground, Text, StyleSheet, View, TextInput, TouchableOpacity, Platform } from 'react-native';
 
 import backgroundImage from '../../assets/imgs/login.jpg';
 import commonStyles from '../commonStyles';
 
 export default class Auth extends Component {
- render(){
-   return(
-    <ImageBackground style={ styles.background } source={ backgroundImage } >
-      <Text style={ styles.title } >Tasks</Text>
-    </ImageBackground>
-   );
- } 
+  state = {
+    email: '',
+    password: '',
+  }
+
+  render() {
+    return (
+      <ImageBackground style={styles.background} source={backgroundImage} >
+        <Text style={styles.title} >Tasks</Text>
+        <View style={ styles.formContainer } >
+          <TextInput
+            placeholder='E-mail'
+            value={ this.state.email }
+            style={ styles.input }
+            onChangeText={ email => this.setState({ email }) }
+          />
+          <TextInput
+            placeholder='Senha'
+            value={ this.state.password }
+            style={ styles.input }
+            onChangeText={ password => this.setState({ password }) }
+          />
+          <TouchableOpacity>
+            <View style={ styles.button } >
+              <Text style={ styles.buttonText } >Entrar</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -26,5 +50,26 @@ const styles = StyleSheet.create({
     color: commonStyles.colors.secundary,
     fontSize: 70,
     marginBottom: 10,
+  },
+  formContainer: {
+    backgroundColor: 'rgba(0, 0, 0, 0.8)',
+    padding: 20,
+    width: '90%',
+  },
+  input: {
+    backgroundColor: '#fff',
+    marginTop: 10,
+    padding: Platform.OS === 'ios' ? 15 : 10,
+  },
+  button: {
+    backgroundColor: '#080',
+    marginTop: 10,
+    padding: 10,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontFamily: commonStyles.fontFamily,
+    color: '#fff',
+    fontSize: 20,
   }
 });
