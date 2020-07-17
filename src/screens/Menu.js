4672bac925a7a15b2496aa0d945ede87e1395b46
Félix Vicente <felixvicent1306@gmail.com>
2020-07-17 14:33:04 -0300

    Customize user info

--- src/screens/Menu.js
+++ src/screens/Menu.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { ScrollView, View, Text, StyleSheet, Platform } from 'react-native';
 import { DrawerItems } from 'react-navigation-drawer';
 import { Gravatar } from 'react-native-gravatar';
+import commonStyles from '../commonStyles';
 
 export default props => {
   return(
     <ScrollView>
       <View style={ styles.header } >
+        <Text style={ styles.title } >Tasks</Text>
         <Gravatar
           style={ styles.avatar }
           options={{
@@ -15,8 +17,8 @@ export default props => {
           }}
         />
         <View style={ styles.userInfo } >
-          <Text>{ props.navigation.getParam('email') }</Text>
-          <Text>{ props.navigation.getParam('name') }</Text>
+          <Text style={ styles.name }>{ props.navigation.getParam('name') }</Text>
+          <Text style={ styles.email }>{ props.navigation.getParam('email') }</Text>
         </View>
       </View>
       <DrawerItems { ...props } />
@@ -29,12 +31,34 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: '#ddd',
   },
+  title: {
+    color: '#000',
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 30,
+    paddingTop: Platform.OS === 'ios' ? 70 : 30,
+    padding: 10,
+  },
   avatar: {
     width: 60,
     height: 60,
     borderWidth: 3,
     borderRadius: 30,
     margin: 10,
-    marginTop: Platform.OS === 'ios' ? 50 : 10,
+    backgroundColor: '#222',
+  },
+  userInfo: {
+    marginLeft: 10,
+  },
+  name: {
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 20,
+    marginBottom: 5,
+    color: commonStyles.colors.mainText,
+  },
+  email: {
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 15,
+    color: commonStyles.colors.subText,
+    marginBottom: 10,
   }
 });
